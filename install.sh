bed64368d76cfb9a12cbe7b0d59d2d3efb7d60b2
Daniele Ieva <daniele.ieva@hotmail.com>
2024-09-26 17:33:27 +0200

    feat: added dependencies to install script

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
 
+# install dependencies (using brew)
+# Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install node neovim gcc luarocks
+
+
 cp -r ./nvim/ $HOME/.config/
