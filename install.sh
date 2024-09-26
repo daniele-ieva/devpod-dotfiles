8b3b5b2bb59fea01a19bcc847fab0fa83cc82e23
Daniele Ieva <daniele.ieva@hotmail.com>
2024-09-26 17:39:53 +0200

    feat: added homebrew setup to install script

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@
 # Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+printf '%s\n' '' 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/vscode/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install node neovim gcc luarocks
 
 
