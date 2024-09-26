928be499b479ee735a8e329ac77507683bb70039
Daniele Ieva <daniele.ieva@hotmail.com>
2024-09-26 17:47:57 +0200

    feat: added check to install script

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
 
-# install dependencies (using brew)
-# Brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-printf '%s\n' '' 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/vscode/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-brew install node neovim gcc luarocks
-
+# Check if dependencies are already installed 
+if [ ! $(which brear 2> /dev/null) ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    printf '%s\n' '' 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/vscode/.bashrc
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    brew install node neovim gcc luarocks
+fi
 
 cp -r ./nvim/ $HOME/.config/
